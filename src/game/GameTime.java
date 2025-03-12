bc4f971572424a878089782f1857038949adf1c3
RaviS993 <ravisrinivasan93@gmail.com>
2025-03-12 11:42:33 -0400

    Updated GameTime.java

--- src/game/GameTime.java
+++ src/game/GameTime.java
@@ -33,6 +33,7 @@ public class GameTime extends BorderPane {
 	private RadioButton option2;
 	private RadioButton option3;
 	private RadioButton option4;
+	private RadioButton option5;
 	
 	private Button btnSubmit;
 	
@@ -40,23 +41,30 @@ public class GameTime extends BorderPane {
 	private Label correct2;
 	private Label correct3;
 	private Label correct4;
+	private Label correct5;
 	
 	private Label youAnswered1;
 	private Label youAnswered2;
 	private Label youAnswered3;
 	private Label youAnswered4;
+	private Label youAnswered5;
 	
 	private Label correctAnswer1;
 	private Label correctAnswer2;
 	private Label correctAnswer3;
 	private Label correctAnswer4;
+	private Label correctAnswer5;
 	
 	private Label gameFinished;
 	
 	private HBox backAndExitButtonsBox;
 	private VBox bodyBox;
 	
+	private String title;
+	
 	public GameTime() {
+		
+		// Initializations
 		
 		btnBack = new Button();
 		btnExit = new Button();
@@ -68,6 +76,71 @@ public class GameTime extends BorderPane {
 		btnVeg = new Button();
 		btnPsych = new Button();
 		btnGeo = new Button();
+		
+		option1 = new RadioButton();
+		option2 = new RadioButton();
+		option3 = new RadioButton();
+		option4 = new RadioButton();
+		option5 = new RadioButton();
+		
+		btnSubmit = new Button();
+		
+		correct1 = new Label();
+		correct2 = new Label();
+		correct3 = new Label();
+		correct4 = new Label();
+		correct5 = new Label();
+		
+		youAnswered1 = new Label();
+		youAnswered2 = new Label();
+		youAnswered3 = new Label();
+		youAnswered4 = new Label();
+		youAnswered5 = new Label();
+		
+		correctAnswer1 = new Label();
+		correctAnswer2 = new Label();
+		correctAnswer3 = new Label();
+		correctAnswer4 = new Label();
+		correctAnswer5 = new Label();
+		
+		gameFinished = new Label();
+		
+		backAndExitButtonsBox = new HBox();
+		bodyBox = new VBox();
+		
+		this.title = "Trivia Time!";
+		
+		styleScreen();
+		show();
+		createGameListeners();
+		
+	}
+	
+	public void styleScreen() {
+		
+		backAndExitButtonsBox.setSpacing(20);
+		backAndExitButtonsBox.setAlignment(Pos.CENTER);
+		backAndExitButtonsBox.setPadding(new Insets(0, 0, 0, 0));
+		
+		bodyBox.setSpacing(20);
+		bodyBox.setAlignment(Pos.CENTER);
+		bodyBox.setPadding(new Insets(0, 0, 0, 0));
+		
+	}
+	
+	public void show() {
+		
+		Stage stage = new Stage();
+		
+		//The usual
+		Scene scene = new Scene(this, 700, 350);
+		stage.setTitle(title);
+		stage.setScene(scene);
+		stage.show();
+		
+	}
+	
+	private void createGameListeners() {
 		
 	}
 	
